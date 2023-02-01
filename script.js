4d879e828bb98eea0ba077f5396cf91f777a82c0
mkouelambou3 <mike.kouelambou@student.rocva.nl>
2023-02-01 15:18:18 +0100

    commit code

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-jQuery(function ($) {
-    $('.ghostly').animate({
-        opacity: '1'
-    }, 3000);
-   
-        $.when($("body").fadeOut(4000).promise(), $.ajax({
-            url: "home.html",
-            type: 'get',
-            dataType: 'html'
-        })).done(function (_html) {
-            $("body").fadeOut(1000, function(){redirectPage("home.html")});
-            $("body").css({
-                display: "none"
-            });
-            $("body").fadeIn(4000);
-        });
-
-});
-
-
 // navbar togglemenu start
 var navLinks = document.getElementById("navLinks");
 

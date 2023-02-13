08faba3b9b0e2d27c60542646816f660664017f6
mkouelambou3 <mike.kouelambou@student.rocva.nl>
2023-02-13 11:38:40 +0100

    save code

--- signup.php
+++ signup.php
@@ -1,2 +1,21 @@
+<html>
+<head>
+<meta charset="UTF-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>POC Share Wheels - Contact Form</title>
+         <link rel="stylesheet" href="contact-style.css">
+         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+         <script src="https://kit.fontawesome.com/d6dc7c8001.js" crossorigin="anonymous"></script>
+</head>
+<nav>
+                <a href="home.html" class="logo">
+                       <i class="fa-solid fa-car-side"></i> POC Share Wheels
+                </a>
+         </nav>
+</html>
+
+
 <?php
+
 ?>

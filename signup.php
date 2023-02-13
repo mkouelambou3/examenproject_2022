0a453f0e148c682ff59a34c54f59589accf42b10
mkouelambou3 <mike.kouelambou@student.rocva.nl>
2023-02-13 13:13:59 +0100

    save code

--- signup.php
+++ signup.php
@@ -3,16 +3,27 @@
 <meta charset="UTF-8">
          <meta http-equiv="X-UA-Compatible" content="IE=edge">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <title>POC Share Wheels - Contact Form</title>
-         <link rel="stylesheet" href="contact-style.css">
+         <title>POC Share Wheels - Confirmation Account</title>
          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
          <script src="https://kit.fontawesome.com/d6dc7c8001.js" crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="contact-style.css">
 </head>
 <nav>
-                <a href="home.html" class="logo">
+       <a href="home.html" class="logo">
                        <i class="fa-solid fa-car-side"></i> POC Share Wheels
                 </a>
          </nav>
+         <div class="container-confirm">
+              <form class="form-confirm"> 
+                <h1>Confirmation Account</h1>
+                <p>Uw account is geregistreerd. Check uw inbox.</p>
+                <div>
+                <a href="home.html" class="hero_btn-confirm" style="
+                margin-left: 35%;
+                ">Send Confirmation Link</a>
+                </div>
+                </form>
+  </div>
 </html>
 
 

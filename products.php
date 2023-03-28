cf3e117a9dbba86cf07b208dee89053eeeb9a661
mkouelambou3 <71011825+mkouelambou3@users.noreply.github.com>
2023-03-28 14:41:35 +0200

    save code

--- products.php
+++ products.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -51,7 +53,7 @@
               </nav>
               <h1>Onze Producten</h1>
 </section>
-
+</body>
 <main role="main">
   <header class="page-header">
     <div class="site-content-wrap site-content-wrap_small">
@@ -67,110 +69,7 @@
             </div>
          </div>
     </div>
-          <div class="content-blocks">
-             <div>
-               <div class="site-content-wrap">
-                  <section class="car-finder card car-finder_loaded">
-                     <form>
-                        <fieldset>
-                            <div class="main-categories_toggle">
-                               <legend class="visually-hidden">Voertuigtype</legend>
-                              <div class="main-categories_toggle-btn">
-                                <input type="radio" id="car" name="car-category" class="visually-hidden" value="car">
-                                <label for="car" class="image-loaded">
-                                   <img width="150" height="84" class="lazy-img loaded" alt="auto" 
-                                   src="car-products/renault_captur_auto.png">
-                                   <span>Auto</span>
-                                </label>
-                              </div>
-                              <div class="main-categories_toggle-btn">
-                                 <input type="radio" id="van" name="main-category" class="visually-hidden" value="van">
-                                 <label for="van" class="image-loaded">
-                                   <img width="150" height="76" class="lazy-img loaded" alt="bestelauto"
-                                   src="car-products/opel_combo_bestelwagen.png">
-                                   <span>Bestelauto</span>
-                                 </label>
-                              </div>
-                              <div class="main-categories_toggle-btn">
-                                <input type="radio" id="lux-car" name="main-category" class="visually-hidden" value="luxary-car">
-                                <label for="lux-car" class="lazy-img loaded">
-                                   <img width="150" height="84" class="lazy-img loaded" alt="luxary-car"
-                                   src="car-products/porsche_taycan.png">
-                                   <span>Luxe Auto</span>
-                                </label>
-                              </div>
-                            </div>
-                        </fieldset>
-                        <div class="car-finder_content">
-                           <div class="car-finder_title">
-                            <p class="car-finder_heading"> 
-                            Huur een 
-                            <span>auto</span>
-                            </p>
-                           </div>
-                           <div class="car-finder_column">
-                             <p class="car-finder_subheading">Waar wil je ophalen?
-                             </p>
-                              <div class="branch-input">
-                                <button type="button" class="btn-form-input">
-                                   <svg class="icon-loaction">
-                                     <use></use>
-                                   </svg>
-                                     <span class="btn-form-input_placeholder">Kies een stad, 
-                                   postcode of plaats</span>
-                                   </svg>
-                                </button>
-                            <div class="branch-input_modal">
-                              <div class="btn-form-input">
-                                <svg class="icon-loaction">
-                                   <use></use>
-                                </svg>
-                              </div>
-                              <input type="text" class="branch-input_search btn-form-input" id="waar-huren" autocomplete="off"
-                              data-form-type="address,zip,query">
-                              <button type="button" aria-label="Popup sluiten"
-                              class="branch-input_btn-close">
-                                <svg class="icon-close">
-                                   <use></use>
-                                </svg>
-                              </button>
-                            </div>
-                              </div>
-                        </div>
-                     <div class="car-finder_column car-finder_calendar">
-                       <p class="car-finder_subheading">Wanneer wil je huren?
-                       </p>
-                       <button type="button" class="btn-form-input" data-dashlane-label="true" data-form-type="action,search">
-                          <svg class="icon-calendar">
-                            <use>
-                              </use>
-                          </svg>
-                           <span class="btn-form-input_placeholder">Kies data en
-                            tijden
-                           </span>
-                       </button>
-                     </div>
-                     <div class="car-finder_column">
-                       <button type="submit" class="btn-primary btn-large btn-block" data-dashlane-label="true" data-form-type="action,search">
-                         Bekijk
-                         <span>auto's</span>
-                       </button>
-                         </div>
-                       </div>
-                     </div>
-                     </form>
-                  </section>
-               </div>
-             </div>
-             <!--<div></div>
-             <div></div>
-             <div></div>
-             <div></div>
-             <div></div>
-             <div></div>
-             !-->
-          </div>
-  </header>   
+  </header>  
 </main>
 
 
@@ -187,6 +86,7 @@
 
 
        <script src="script.js"></script>
+</body>
 </body>
 </html>
 

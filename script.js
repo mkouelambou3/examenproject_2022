b3f28dad1f32211b16931c57475209a3f2c06075
mkouelambou3 <71011825+mkouelambou3@users.noreply.github.com>
2023-04-06 12:59:40 +0200

    save code

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function doStuff (callback) {
     document.body.className = 'visible';
   });
 
+var span = document.getElementById("category-type-text");
+var btn = document.getElementById("submit-btn");
+
+function vanFunction() {
+  span.textContent = "Bestelauto";
+  btn.textContent = "Bekijk bestelauto's";
+}
+
+function carFunction() {
+  span.textContent = "auto";
+  btn.textContent = "Bekijk auto's";
+}
+
+function luxeFunction() {
+  span.textContent = "Luxe auto";
+  btn.textContent = "Bekijk luxe auto's";
+}
+

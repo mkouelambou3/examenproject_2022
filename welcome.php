c78784dde2d56aa7ef7de84fe9428082b6f938ce
mkouelambou3 <71011825+mkouelambou3@users.noreply.github.com>
2023-11-28 13:40:36 +0100

    adjusting + adding new changes to notification center.

--- welcome.php
+++ welcome.php
@@ -60,9 +60,9 @@ if (isset($_POST["finder-btn"])) {
   <script src="https://kit.fontawesome.com/d6dc7c8001.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </head>
 <style>
@@ -73,59 +73,64 @@ if (isset($_POST["finder-btn"])) {
 
 <body>
 
-<section class="header">
+  <section class="header">
     <nav>
-        <a href="#" class="logo">
-            <i class="fa-solid fa-car-side"></i> POC Share Wheels
-        </a>
-        <div class="nav-links" id="navLinks">
-            <i class="fa fa-times" onclick="hideMenu()"></i>
-            <ul>
-                <li><a href="welcome.php">Home</a></li>
-                <li><a href="about.php">Over Ons</a></li>
-                <li><a href="poc-products.php">Producten</a></li>
-                <li style="display: none;"><a href="#">Reserveren</a></li>
-                <div class="dropdown">
-                    <li><a href="#">Account</a></li>
-                    <div class="dropdown-content">
-                        <div class="user-info">
-                          <?php echo "Welcome, "  . $row['naam']; ?>
-                        </div>
-                        <!-- Move the "Berichten" button inside the account dropdown -->
-                        <button id="berichtenButton" onclick="openModal()">
-                          Berichten
-                          <span class="counter-circle" id="counter">0</span>
-                      </button>
-                        <a href="profile.php">Profiel</a>
-                        <a href="logout.php">Uitloggen</a>
-                        <!-- Move the "Berichten" button outside the account dropdown -->
-                    </div>
-                </div>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <i class="fa fa-bars" onclick="showMenu()"></i>
+      <a href="#" class="logo">
+        <i class="fa-solid fa-car-side"></i> POC Share Wheels
+      </a>
+      <div class="nav-links" id="navLinks">
+        <i class="fa fa-times" onclick="hideMenu()"></i>
+        <ul>
+          <li><a href="welcome.php">Home</a></li>
+          <li><a href="about.php">Over Ons</a></li>
+          <li><a href="poc-products.php">Producten</a></li>
+          <li style="display: none;"><a href="#">Reserveren</a></li>
+          <div class="dropdown">
+            <li><a href="#">Account</a></li>
+            <div class="dropdown-content">
+              <div class="user-info">
+                <?php echo "Welcome, "  . $row['naam']; ?>
+              </div>
+              <!-- Move the "Berichten" button inside the account dropdown -->
+              <button id="berichtenButton" onclick="openModal()">
+                Berichten
+                <span class="counter-circle" id="counter">0</span>
+              </button>
+              <a href="profile.php">Profiel</a>
+              <a href="logout.php">Uitloggen</a>
+              <!-- Move the "Berichten" button outside the account dropdown -->
+            </div>
+          </div>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <i class="fa fa-bars" onclick="showMenu()"></i>
     </nav>
-</section>
+  </section>
 
-<!-- Modal HTML structure -->
-<div class="modal" id="berichtenModal" tabindex="-1" role="dialog">
+  <!-- Modal HTML structure -->
+  <div class="modal" id="berichtenModal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Berichten</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="closeModal()">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Add content for your modal here -->
-                <p class="messages-subtitle_text">You have <span id="modalCounter">0</span> new messages.</p>
-                <!-- Add additional content as needed -->
-            </div>
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Berichten</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="closeModal()">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+        <div class="modal-body">
+          <!-- Add content for your modal here -->
+          <p class="messages-subtitle_text">You have <span id="modalCounter">0</span> new messages.</p>
+          <!-- Add additional content as needed -->
+        </div>
+
+        <div class="modal-footer">
+          <p>Huidige versie: 1.0.16.3</p>
+          <p>u bent up-to-date.</p>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 
 
   <div class="text_box">
